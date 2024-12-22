1f51878b5f3fe68859f3fc68e54404bd32af8888
ez1n <eodrmfdl1004@naver.com>
2024-12-22 18:38:26 +0900

    chore: axios 인스턴스 생성

---
+++ src/_networks/axios.config.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+  withCredentials: true,
+});
+
+export { api };
