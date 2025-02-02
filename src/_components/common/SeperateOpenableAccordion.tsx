119440a42b83345ac8f429488e9be96c6563ec22
Seong Heung Bae <mopil1102@gmail.com>
2025-02-02 20:00:41 +0900

    feat: 타입 강제 캐스팅 추가

--- src/_components/common/SeperateOpenableAccordion.tsx
+++ src/_components/common/SeperateOpenableAccordion.tsx
@@ -3,6 +3,7 @@ import {
   Accordion,
   AccordionHeader,
   AccordionBody,
+  AccordionProps,
 } from '@material-tailwind/react'
 import { IcoAccordionArrow } from '../icons'
 
@@ -19,16 +20,18 @@ export function SeperateOpenableAccordion(props: SeperateOpenableAccordion) {
   const { title, defaultOpen, children } = props
   const [isOpen, setIsOpen] = useState(defaultOpen)
 
-  // TODO: props 인식 못하는 에러 수정 필요
+  // TODO: props 인식 못하는 에러 수정 필요 (any 타입 캐스팅 제거)
   return (
     <Accordion
       className="rounded-lg"
       open={isOpen}
       icon={<IcoAccordionArrow isOpened={isOpen} />}
+      {...({} as any)}
     >
       <AccordionHeader
         className="p-2"
         onClick={() => setIsOpen((prev) => !prev)}
+        {...({} as any)}
       >
         {title}
       </AccordionHeader>
