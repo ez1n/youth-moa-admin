db4a25f11a06e756f8438324241e8c14cbd76325
Seong Heung Bae <mopil1102@gmail.com>
2025-02-09 13:49:27 +0900

    refactor: children 없는경우 닫힘 태그 제거

--- src/app/users/[id]/components/Row.tsx
+++
@@ -1,3 +0,0 @@
-export const Row = ({ children }: { children: React.ReactNode }) => {
-  return <tr>{children}</tr>
-}
--- src/app/users/[id]/components/UserInfoDetail.tsx
+++ src/app/users/[id]/components/UserInfoDetail.tsx
@@ -3,7 +3,6 @@ import { Radio } from '@/_components/common/Radio'
 import { Button } from '@/_components/common/Button'
 import { IcoSearch } from '@/_components/icons'
 import { DatePicker } from '@/_components/common/DatePicker'
-import { Row } from '../components/Row'
 import { Cell, CellLabel } from '../components/Cell'
 import { SearchAddress } from '@/_components/common/SearchAddress'
 
@@ -22,60 +21,57 @@ export const UserInfoDetail = () => {
       <h2 className="text-xl font-bold mb-4 py-5 px-10 my-3">회원정보</h2>
       <table className="w-full border-separate border-spacing-y-4 px-8">
         <tbody className="">
-          <Row>
+          <tr>
             <CellLabel label="아이디" />
             <Cell>
-              <Input disabled></Input>
+              <Input disabled />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <CellLabel label="비밀번호" />
             <Cell>
-              <Input
-                type="password"
-                placeholder="비밀번호를 입력해주세요"
-              ></Input>
+              <Input type="password" placeholder="비밀번호를 입력해주세요" />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <CellLabel label="비밀번호 확인" />
             <Cell>
               <Input
                 type="password"
                 placeholder="비밀번호를 한번 더 입력해주세요"
-              ></Input>
+              />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <CellLabel label="이름" />
             <Cell>
-              <Input></Input>
+              <Input />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <CellLabel label="권한" />
             <Cell className="flex">
               <Radio disabled name="role" label="사용자" />
               <Radio disabled name="role" label="관리자" />
               <Radio disabled name="role" label="시스템관리자" />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <CellLabel label="핸드폰 번호" />
             <Cell>
-              <Input></Input>
+              <Input />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <CellLabel label="주소" />
             <Cell>
-              <Input disabled value={address}></Input>
+              <Input disabled value={address} />{' '}
             </Cell>
             <Cell>
               <Button
@@ -86,42 +82,42 @@ export const UserInfoDetail = () => {
                 재검색
               </Button>
             </Cell>
-          </Row>
-          <Row>
+          </tr>
+          <tr>
             <Cell> </Cell>
             <Cell>
-              <Input placeholder="상세주소"></Input>
+              <Input placeholder="상세주소" />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <CellLabel label="성별" />
             <Cell className="flex justify-between">
               <Radio name="gender" label="남" />
               <Radio name="gender" label="여" />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <CellLabel label="생년월일" />
             <Cell>
-              <DatePicker></DatePicker>
+              <DatePicker />
             </Cell>
-          </Row>
+          </tr>
 
-          <Row>
+          <tr>
             <Cell> </Cell>
             <Cell className="px-12">
               <Button onClick={console.log}>저장</Button>
             </Cell>
-          </Row>
+          </tr>
         </tbody>
       </table>
       {searchAddressPopup && (
         <SearchAddress
           setAddress={setAddress}
           handleComplete={handleSearchAddressPopup}
-        ></SearchAddress>
+        />
       )}
     </section>
   )
