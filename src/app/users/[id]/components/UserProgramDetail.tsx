3e0c5195c4ce678eb14a5ecca6848a72cf1aa5fd
Seong Heung Bae <mopil1102@gmail.com>
2025-02-09 13:15:34 +0900

    feat: 신청 내역이 없을 때 추가

--- src/app/users/[id]/components/UserProgramDetail.tsx
+++ src/app/users/[id]/components/UserProgramDetail.tsx
@@ -40,15 +40,16 @@ export const UserProgramDetail = () => {
   const allApplications = dummyCards
   const [applications, setApplications] = useState(allApplications)
   const [tabAnimate, setTabAnimate] = useState(false)
+  const [isEmpty, setIsEmpty] = useState(false)
 
   useEffect(() => {
     setTabAnimate(true)
-    setApplications(
-      allApplications.filter((application) => {
-        if (selected === '전체') return true
-        return application.programApplicationStatus === selected
-      })
-    )
+    const filtered = allApplications.filter((application) => {
+      if (selected === '전체') return true
+      return application.programApplicationStatus === selected
+    })
+    setApplications(filtered)
+    setIsEmpty(filtered.length === 0)
   }, [selected])
 
   useEffect(() => {
@@ -61,39 +62,60 @@ export const UserProgramDetail = () => {
 
   return (
     <section>
-      <section className="border rounded-xl flex p-4 justify-center items-center shadow-md">
-        <h2 className="text-xl font-bold mb-4 py-5 px-10 my-3">
-          프로그램 신청 현황
-        </h2>
-        <div className="flex gap-2 p-4 rounded-lg">
-          {프로그램신청현황상태.map((option) => (
-            <RadioButton
-              key={option}
-              label={option}
-              value={option}
-              name="status"
-              defaultChecked={selected === option}
-              onChange={(e) => setSelected(e.target.value)}
-            />
-          ))}
-        </div>
-      </section>
-      <section className="max-h-[800vh] overflow-y-auto">
-        <div
-          className={`transition-transform duration-100 ease-out ${
-            tabAnimate
-              ? 'transform translate-y-0 opacity-0'
-              : 'transform translate-y-2 opacity-100'
-          }`}
-        >
-          {applications.map((application) => (
-            <ProgramApplicationCard
-              key={application.applicationId}
-              {...application}
-            />
-          ))}
-        </div>
-      </section>
+      {isEmpty && (
+        <section className="border rounded-xl p-4 flex flex-col justify-center items-center shadow-md">
+          <h2 className="text-xl font-bold mb-4 py-5 px-10 my-3">
+            프로그램 신청 현황
+          </h2>
+          <img
+            className=""
+            src="/no_applications.png"
+            alt={'no_application'}
+            width={50}
+            height={50}
+          ></img>
+          <div className="mx-20 text-gray-500 my-5">
+            <p className="text-center">신청된 프로그램이 없습니다.</p>
+          </div>
+        </section>
+      )}
+      {!isEmpty && (
+        <>
+          <section className="border rounded-xl flex p-4 justify-center items-center shadow-md">
+            <h2 className="text-xl font-bold mb-4 py-5 px-10 my-3">
+              프로그램 신청 현황
+            </h2>
+            <div className="flex gap-2 p-4 rounded-lg">
+              {프로그램신청현황상태.map((option) => (
+                <RadioButton
+                  key={option}
+                  label={option}
+                  value={option}
+                  name="status"
+                  defaultChecked={selected === option}
+                  onChange={(e) => setSelected(e.target.value)}
+                />
+              ))}
+            </div>
+          </section>
+          <section className="max-h-[800vh] overflow-y-auto">
+            <div
+              className={`transition-transform duration-100 ease-out ${
+                tabAnimate
+                  ? 'transform translate-y-0 opacity-0'
+                  : 'transform translate-y-2 opacity-100'
+              }`}
+            >
+              {applications.map((application) => (
+                <ProgramApplicationCard
+                  key={application.applicationId}
+                  {...application}
+                />
+              ))}
+            </div>
+          </section>
+        </>
+      )}
     </section>
   )
 }
